a6009144876c0625b82a5eef07946ea555f35f91
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-03-29 21:35:49 +0800

    Eureka 单机

--- cloud-eureka-server7001/src/main/java/com/auguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/auguigu/springcloud/EurekaMain7001.java
@@ -1,21 +1,21 @@
-package com.atguigu.springcloud;
-
+package com.auguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author xiguozh
  * @version 1.0
  * @description
- * @date 2021-03-26 09-59
+ * @date 2021-03-29 17-05
  * @since 1.8
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
