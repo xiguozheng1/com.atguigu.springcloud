977d5f57dc08be0c8668dca84865e863895e98d2
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-03-30 14:39:34 +0800

    初级阶段完成 注册和服务端，集群

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -1,6 +1,5 @@
 package com.atguigu.springcloud;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,14 +8,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author xiguozh
  * @version 1.0
  * @description
- * @date 2021-03-26 09-59
+ * @date 2021-03-30 14-12
  * @since 1.8
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class PaymentMain8001 {
+public class PaymentMain8002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(PaymentMain8002.class, args);
     }
 }
