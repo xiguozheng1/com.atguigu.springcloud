8d42271f7456a43b57d670c284270ec48ca7a92b
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-04-23 17:00:00 +0800

    Config 配置

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author xiguozh
+ * @version 1.0
+ * @description
+ * @date 2021-04-23 16-47
+ * @since 1.8
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
