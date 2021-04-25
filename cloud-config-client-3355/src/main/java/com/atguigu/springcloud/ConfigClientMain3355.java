acc5263dc2892eb9bd87f17d3311d8ad7bc599f1
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-04-25 13:39:43 +0800

    Config客户端配置

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xiguozh
+ * @version 1.0
+ * @description
+ * @date 2021-04-25 11-11
+ * @since 1.8
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiguozh
+ * @version 1.0
+ * @description
+ * @date 2021-04-25 11-14
+ * @since 1.8
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
