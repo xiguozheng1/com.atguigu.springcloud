9a5fd4bf5d4b88f3c6f051304ef732af80ea5edd
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-04-25 15:06:13 +0800

    Config（手动动态刷新）动态刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 1.8
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
