e8d52457bbed2d2f2ad0c4fc7b21a81797892f4f
xi_guozheng <guozheng.xi@grandchallenge.co.jp>
2021-04-26 17:18:57 +0800

    springCloud bus 服务总线 动态刷新全局广播

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xiguozh
+ * @version 1.0
+ * @description
+ * @date 2021-04-26 15-05
+ * @since 1.8
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiguozh
+ * @version 1.0
+ * @description
+ * @date 2021-04-25 11-14
+ * @since 1.8
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort" + serverPort + "\t\n\n configInfo" + configInfo;
+    }
+}
